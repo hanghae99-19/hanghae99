6eb9fba179e480a0aece335d721e7630989c5685
jeonghyeonuk <103723109+Jeonghyeonuk@users.noreply.github.com>
2022-04-22 19:25:47 +0900

    Add 정현욱

--- 4주차과제/app.py
+++ 4주차과제/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ryfyy.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
+import certifi
 
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.arjwt.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 
 @app.route('/')
 def home():
@@ -16,18 +19,16 @@ def homework_post():
     comment_receive = request.form['comment_give']
 
     doc = {
-    'name': name_receive,
-    'comment': comment_receive
+        'name':name_receive,
+        'comment':comment_receive
     }
-
-    db.guest_book.insert_one(doc)
-
-    return jsonify({'msg':'저장 완료!'})
+    db.homework.insert_one(doc)
+    return jsonify({'msg':'응원완료!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-    book_list = list(db.guest_book.find({}, {'_id': False}))
-    return jsonify({'guest_book': book_list})
+    homework_list = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'homework':homework_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5500, debug=True)
